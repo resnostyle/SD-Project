028478abf7f6d93ab99dcfcef04989db585ef5a0
BryanWp <bwp.pearson@gmail.com>
2012-04-02 22:08:24 -0400

    initial setup

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    	<link rel="stylesheet" href="CSS/css.php?files=css.cssp" type="text/css" media="screen" />
+
+          
+        <title></title>
+    </head>
+    <body>
+        <div id="header">
+            <div id="navigation">
+                <a href="#">Link</a> | <a href="#">Link</a> | <a href="#">Link</a> |
+            </div>
+        </div>
+
+        <div id="content">
+            <div id="blog">
+            <h1>From the Blog</h1>
+
+
+            </div>
+        </div>
+
+        <div id="footer">
+            <div id="content">
+                <p><a href="#">Twitter</a> <a href="#">Facebook</a> <a href="#">YouTube</a>  | <a href="#">Contact Us</a> | <a href="#">Privacy</a></p>
+                <p>C 2012 SchoolDude.com - All Rights Reserved</p>
+            </div>
+        </div>
+    </body>
+</html>
