b16f9b04c6b93c4cdbac946907753d96b1b5e660
BryanWp <bwp.pearson@gmail.com>
2012-04-03 22:09:38 -0400

    added flickr and twitter feeds

--- functions/flickr.php
+++ functions/flickr.php
@@ -1,19 +1,50 @@
 <?php
-/** *  * *
- * @package Package Name
- * @subpackage Subpackage
- * @category Category
+/*
+ * @package Flickr Call
  * @author Bryan Pearson <bp at bryanwp>
  * @copyright Apr 3, 2012
  */
 
+$flickr_method = "flickr.people.getPublicPhotos";
+$return_format = "php_serial";
+$apikey = "a374e28362904949107a691d94332f19";
+$userid = "7224150@N06";
 
+//start a curl instance
+$ch = curl_init();
 
+// set URL
+curl_setopt($ch, CURLOPT_URL, "http://www.flickr.com/services/rest/?method=$flickr_method&user_id=$userid&format=$return_format&api_key=$apikey");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+// grab URL and pass it to the browser
+$curl_serialized = curl_exec($ch);
 
+//gather error checking
+$info = curl_getinfo($ch);
 
+// close cURL resource
+curl_close($ch);
 
+//unserialize output from flickr
+$curl_unserialized = unserialize($curl_serialized);
 
+//checking stat from flickr
+if ($curl_unserialized['stat'] != 'ok')
+{
+    echo 'Flickr has returned a bad status.';
+    //@todo log this info for further analysis, likely being rate limited, cache queries?
+    die();
+}
+
+for ($i = 1; $i < 7; $i++)
+{
+    //output image url
+    //@todo abstract the url in a better way, this is messy
+    echo "<img height='100px' src='http://farm".$curl_unserialized['photos']['photo'][$i]['farm'].".static.flickr.com/"
+            .$curl_unserialized['photos']['photo'][$i]['server'].'/'.$curl_unserialized['photos']['photo'][$i]['id'].
+            '_'.$curl_unserialized['photos']['photo'][$i]['secret'].".jpg"."'/>";
+}
 
 /* End of file flickr.php */
-/* Location:  */
---
+++ functions/twitter.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @package Twitter Call
+ * @author Bryan Pearson <bp at bryanwp>
+ * @copyright Apr 3, 2012
+ */
+
+$search_method = 'json';
+$search_term = '%23savingssummit';
+$results_requested = '2';
+$result_type = 'recent';
+
+//start a curl instance
+$ch = curl_init();
+
+// set URL
+curl_setopt($ch, CURLOPT_URL, "http://search.twitter.com/search.$search_method?q=$search_term&rpp=$results_requested&include_entities=false&result_type=$result_type");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// grab URL and pass it to the browser
+$curl_page = curl_exec($ch);
+
+// close cURL resource
+curl_close($ch);
+
+var_dump(json_decode($curl_page));
+/* End of file twitter.php */
