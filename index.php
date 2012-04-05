e2b19ad53f97e2825010429cc802ba52f6e330d7
BryanWp <bwp.pearson@gmail.com>
2012-04-04 21:26:35 -0400

    blog and flickr are a go... now for the rest

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- /*
+/*
  * By: Bryan Pearson
  * Postion: SchoolDude Guy
  * Purpose:
@@ -19,22 +19,23 @@ http://www.youtube.com/watch?v=GemGY0r6gTU&list=UUyQQqjt2BeeBXqwD_SFDj1g&index=2
   * From the Blog: This is a stream from our blog located at : http://www.schooldude.com/blog/.
     We only want to show posts from the Energy category.
  
- */
+*/
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    	<link rel="stylesheet" href="CSS/css.php?files=css.cssp" type="text/css" media="screen" />
-        
+        <link rel="stylesheet" href="CSS/second.css" type="text/css" media="screen" />
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,400italic,700,700italic' rel='stylesheet' type='text/css' />
         <title>SchoolDude Media MashUp</title>
-<?php
+        <?php
+
 //error reporting remember to remove this
-error_reporting(E_ALL);
-error_reporting(-1);
-?>
-        
+//error_reporting(E_ALL | E_STRICT);
+//error_reporting(-1);
+        ?>
+
     </head>
     <body>
         <div id="header">
@@ -45,52 +46,41 @@ error_reporting(-1);
                 <ul id="register">
                     <li><a href="#">Register</a></li>
                 </ul>
-                
+
             </div>
         </div>
 
         <div id="content">
             <div id="mainpanel">
 
-            <div id="flickr">
-                <h1>Summit Snapshots</h1>
-        <?php // include 'functions/flickr.php'; ?>
-                <p><a href="http://www.flickr.com/photos/schooldude" class="button">Check out our Flickr Stream</a></p>
-            </div>
-            <div id="blog">
-            <h1>From the Blog</h1>
+                <div id="flickr">
+                    <h1>Summit Snapshots</h1>
+                    <?php include 'functions/flickr.php'; ?>
+                    <p><a href="http://www.flickr.com/photos/schooldude" class="button">Check out our Flickr Stream</a></p>
+                </div>
+                <div id="blog">
+                    <h1>From the Blog</h1>
 
-            <h2>Blog Post</h2>
-<!-- http://www.schooldude.com/blog/category/energy/feed/ -->
-	<?php include 'functions/blog.php'; ?>
-<?php foreach ($feed->get_items() as $item): ?> 
+                    <?php include 'functions/blog.php'; ?>
 
-<div id="postdetails">
-POSTED <?php echo $item->get_date('F J Y'); ?> | BY <?php echo $item->get_author(); ?> | COMMENTS(0)
-            </div>
-<?php echo  $item->get_description(); ?>
+                    <?php foreach ($feed->get_items() as $item): ?>
 
-<div id="readmore">
-    <a href="<?php echo $item->get_permalink(); ?>">Read the full article</a>
-</div>
+                    <h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
+                    <div id="postdetails">
+                        POSTED <?php echo $item->get_date('F j, Y'); ?> | BY Jill Judd  | COMMENTS(0)
+                    </div>
+                    <p><?php echo  $item->get_description(); ?></p>
 
+                    <div id="readmore">
+                        <a href="<?php echo $item->get_permalink(); ?>" class="underline">Read the full article</a>
+                    </div>
+                    <?php endforeach; ?>
 
-
-
-<?php endforeach; ?>
-
-
-
-
-
-
-
-
-            </div>
+                </div>
             </div>
             <div id="sidepanel">
                 <h1>What people are saying:</h1>
-                
+
                 <h2>On Twitter<span>(Tag: #SavingsSummit)</span></h2>
                 <div id="tweet">
                     <div class="time">1 hour ago</div>
@@ -98,14 +88,14 @@ POSTED <?php echo $item->get_date('F J Y'); ?> | BY <?php echo $item->get_author
                 </div>
                 <p><a href="http://twitter.com/#!/schooldude" class="button">Follow @SchoolDude</a></p>
                 <p><a href="https://twitter.com/#!/search/%23savingssummit" class="greenlink" class="underline">More #SavingsSummit on Twitter</a></p>
-               
+
 
                 <h2>On YouTube</h2>
                 <div class="youtube">
-                <iframe style="float:right" width="240" height="119" src="https://www.youtube.com/embed/GemGY0r6gTU?rel=0" frameborder="0" allowfullscreen></iframe> 
-                
-                <a href="http://www.youtube.com/user/SchoolDudeDotCom" class="greenlink">Watch more videos</a>
-		</div>
+                    <iframe style="float:right" width="240" height="119" src="https://www.youtube.com/embed/GemGY0r6gTU?rel=0" frameborder="0" allowfullscreen></iframe>
+
+                    <a href="http://www.youtube.com/user/SchoolDudeDotCom" class="greenlink">Watch more videos</a>
+                </div>
                 <h2>On Facebook</h2>
 
                 <a href="https://www.facebook.com/SchoolDude" class="button">Like us on Facebook</a>
@@ -113,7 +103,7 @@ POSTED <?php echo $item->get_date('F J Y'); ?> | BY <?php echo $item->get_author
         </div>
 
         <div id="footer">
-               
+
         </div>
     </body>
 </html>
